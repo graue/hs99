4cd12bf674c415aedb02edfa8c47dd1968fe6776
Scott Feeney <scott@oceanbase.org>
2013-04-04 14:58:41 -0400

    Add problem 10

---
+++ P10-rle.hs
@@ -0,0 +1,10 @@
+-- Problem 10: Run-length encoding of a list.
+-- > encode "aaaabccaadeeee"
+-- [(4,'a'),(1,'b'),(2,'c'),(2,'a'),(1,'d'),(4,'e')]
+
+encode :: (Eq a) => [a] -> [(Int, a)]
+encode [] = []
+encode [x] = [(1, x)]
+encode (x:xs) = let sames = takeWhile (\y -> x == y) (x:xs)
+                    numSames = length sames
+                in (numSames, x) : (encode (drop numSames (x:xs)))
