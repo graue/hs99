98d40f9de033a0e79e5de4d0980f0e5cad4ef439
Scott Feeney <scott@oceanbase.org>
2013-04-06 02:18:48 -0400

    Add problem 11

---
+++ P11-modrle.hs
@@ -0,0 +1,18 @@
+-- Problem 11: Modified run-length encoding of a list.
+-- If an element has no duplicates it is simply copied.
+-- > encodeModified "aaaabccaadeeee"
+-- [Multiple 4 'a',Single 'b',Multiple 2 'c',
+--  Multiple 2 'a',Single 'd',Multiple 4 'e']
+
+data RLEElement a = Multiple Int a | Single a
+    deriving(Show)
+
+encodeModified :: (Eq a) => [a] -> [RLEElement a]
+encodeModified [] = []
+encodeModified (x:xs) = let sames = takeWhile (\y -> x == y) (x:xs)
+                            numSames = length sames
+                            encodeSames = if numSames == 1
+                                            then Single x
+                                            else Multiple numSames x
+                        in encodeSames :
+                            (encodeModified $ drop numSames (x:xs))
